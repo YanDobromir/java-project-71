3d70542dd4472c5cde13312abb902ebeb2df2a88
YanDobromir <yandobromir@gmail.com>
2024-11-19 22:13:40 +0100

    preview features

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,7 +3,18 @@ package hexlet.code;
 import java.util.concurrent.Callable;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
+@Parameters(index = "0", description = "path to first file")
+    private String filepath1;
+@Parameters(index = "1", description = "path to second file")
+    private String filepath2;
+@Option(
+        names = {"-f", "--format"},
+        description = "output format [default: stylish]",
+        defaultValue = "stylish"
+)
 @Command(
     name = "gendiff",
     mixinStandardHelpOptions = true,
